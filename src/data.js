c2ba67ddd93e8e968f25110fdf7de7c058df2611
Chintan <cgandhi4@gmail.com>
2022-08-15 21:40:24 +0530

    Categories data

---
+++ src/data.js
@@ -0,0 +1,29 @@
+const CATEGORIES = [
+ {
+    id: "1",
+    title: "pencils",
+    imageUrl: "/images/pencils.jpg"
+ }, 
+ {
+    id: "2",
+    title: "folders",
+    imageUrl: "/images/folders.jpg",
+ },
+ {
+    id: "3",
+    title: "diaries",
+    imageUrl: "/images/diaries.jpg",
+ },
+ {
+    id: "4",
+    title: "paints",
+    imageUrl: "/images/paints.jpg",
+ },
+ {
+    id: "5",
+    title: "gifts",
+    imageUrl: "/images/gifts.jpg",
+ }
+]
+
+export default CATEGORIES
