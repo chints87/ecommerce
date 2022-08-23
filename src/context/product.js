36d4c979ee73f2da231b727a13315132eb8a1e89
Chintan <cgandhi4@gmail.com>
2022-08-23 13:26:31 +0530

    After sucessful write of data to firestore

--- src/context/product.js
+++ src/context/product.js
@@ -1,6 +1,6 @@
-import { createContext, useState, useEffect } from 'react';
+import { createContext, useState } from 'react';
 import {addCollectionAndDocuments } from '../utilities/firebase/firebase'
-import SHOP_DATA from '../shop-data.js'
+/* import SHOP_DATA from '../shop-data.js' */
 
 export const ProductContext = createContext({
     products: [],
@@ -9,9 +9,11 @@ export const ProductContext = createContext({
 
 export const ProductProvider = ({ children }) => {
   const[products, setProducts] = useState([]);
-  useEffect(() => {
+//   To upload static data from front end. Should be done only
+//   once
+  /* useEffect(() => {
     addCollectionAndDocuments('categories', SHOP_DATA)
-  },[])
+  },[]) */
     return (
         <ProductContext.Provider value={{products, setProducts}}>
             {children}
