7b700efdfedd0d68595b264700bdd8209426957f
Chintan <cgandhi4@gmail.com>
2022-08-24 18:01:01 +0530

    Add createSelectors for categorySelector

--- src/routes/CategoriesPreview/CategoriesPreview.js
+++ src/routes/CategoriesPreview/CategoriesPreview.js
@@ -1,13 +1,13 @@
 // import { useContext } from 'react';
 // import { CategoryContext } from '../../context/category';
 import { useSelector } from 'react-redux/es/exports';
-import { categoriesMap } from '../../store/categories/categorySelector';
+import { selectCategoriesMap } from '../../store/categories/categorySelector';
 import CategoryPreview from '../../components/CategoryPreview/CategoryPreview';
 
 
 function CategoriesPreview() {
   // const { categories } = useContext(CategoryContext);  
-  const categories = useSelector(categoriesMap)
+  const categories = useSelector(selectCategoriesMap)
   return (
     <>
      {Object.keys(categories).map((title) => {
