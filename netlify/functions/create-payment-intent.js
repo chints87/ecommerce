e82c8564f86cfcf121ef2de38e26933fd76c06b0
Chintan <cgandhi4@gmail.com>
2022-09-01 16:15:45 +0530

    Update create-intent-fuction

--- netlify/functions/create-payment-intent.js
+++ netlify/functions/create-payment-intent.js
@@ -3,17 +3,32 @@ const stripe = require("stripe")(process.env.STRIPE_SECRET_KEY)
 
 exports.handler = async(event) => {
     try{
-      const { amount} = JSON.parse(event.body)
+      const { amount, name} = JSON.parse(event.body)
+
+      const customer = await stripe.customers.create({
+        name: name,
+        address: {
+          line1: '510 Townsend St',
+          postal_code: '98140',
+          city: 'San Francisco',
+          state: 'CA',
+          country: 'US',
+        },
+      });
 
       const paymentIntent = await stripe.paymentIntents.create({
         amount,
         currency: "usd",
-        payment_method_types:["card"]
+        payment_method_types:["card"],
+        description: 'test',
+        customer: customer.id
       })
+
+      
 
       return{
         statusCode: 200,
-        body: JSON.stringify({ paymentIntent })
+        body: JSON.stringify({ paymentIntent, customer })
       }
 
     }catch(error){
