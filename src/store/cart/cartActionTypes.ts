c7735abfbcddb5182c98f813099f643b7a33dee2
Chintan <cgandhi4@gmail.com>
2022-09-06 14:41:38 +0530

    Update cart store to TS

--- src/store/cart/cartActionTypes.ts
+++ src/store/cart/cartActionTypes.ts
@@ -1,4 +1,10 @@
+import { CategoryItem } from "../categories/categoriesActionTypes";
+
 export enum CART_ACTION_TYPES {
     TOGGLE_CART_ICON = 'TOGGLE_CART_ICON',
     UPDATE_CART_ITEMS = 'UPDATE_CART_ITEMS'    
+}
+
+export type CartItem = CategoryItem & {
+    quantity : number;
 }
