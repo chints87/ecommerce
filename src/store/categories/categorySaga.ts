899e1ccca0a712fd1fa7d4775fe05afb80ec1c8a
Chintan <cgandhi4@gmail.com>
2022-09-09 14:58:51 +0530

    Change Saga to TS

--- src/store/categories/categorySaga.ts
+++ src/store/categories/categorySaga.ts
@@ -1,4 +1,4 @@
-import { takeLatest, all, call, put} from 'redux-saga/effects'
+import { takeLatest, all, call, put} from 'typed-redux-saga/macro'
 
 import { getCategoriesAndDocuments } from "../../utilities/firebase/firebase";
 import { CATEGORIES_ACTION_TYPES } from "./categoriesActionTypes";
@@ -8,19 +8,19 @@ import { fetchCategoriesSuccess, fetchCategoriesFailed } from "./categoriesActio
 
 export function* fetchCategoriesAsync() {    
     try {       
-        const categoriesArray = yield call(getCategoriesAndDocuments); 
-        yield put(fetchCategoriesSuccess(categoriesArray));
+        const categoriesArray = yield* call(getCategoriesAndDocuments); 
+        yield* put(fetchCategoriesSuccess(categoriesArray));
         
     } catch (error) {
-        yield put(fetchCategoriesFailed(error))
+        yield* put(fetchCategoriesFailed(error as Error))
     }   
 
 }
 
 export function* onFetchCategories(){
-    yield takeLatest(CATEGORIES_ACTION_TYPES.FETCH_CATEGORIES_DATA_START, fetchCategoriesAsync)
+    yield* takeLatest(CATEGORIES_ACTION_TYPES.FETCH_CATEGORIES_DATA_START, fetchCategoriesAsync)
 }
 
 export function* categoriesSaga(){
-    yield all([call(onFetchCategories)])
+    yield* all([call(onFetchCategories)])
 }
