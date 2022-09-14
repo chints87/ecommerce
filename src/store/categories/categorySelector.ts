5b05a12c8fb40161d53eeabeff9f306550da1bf2
Chintan <cgandhi4@gmail.com>
2022-09-14 12:58:56 +0530

    Add Typescript and Change category store

--- src/store/categories/categorySelector.ts
+++ src/store/categories/categorySelector.ts
@@ -1,6 +1,11 @@
 import { createSelector } from 'reselect';
 
-const selectCategoryReducer = (state) => state.categories
+import { CategoriesState } from './categoriesReducer';
+
+import { CategoryMap } from './categoriesActionTypes';
+import { RootState } from '../store';
+
+const selectCategoryReducer = (state : RootState) : CategoriesState => state.categories
 
 export const selectCategories = createSelector(
     [selectCategoryReducer],
@@ -14,11 +19,11 @@ export const selectCategoriesIsLoading = createSelector(
 
 export const selectCategoriesMap = createSelector(
     [selectCategories],
-    (categories) => categories.reduce((acc,category) => {
+    (categories) : CategoryMap => categories.reduce((acc,category) => {
         const { title, items } = category;
         acc[title.toLowerCase()] = items;
         return acc
-      }, {})
+      }, {} as CategoryMap)
 )
 // export const categoriesMap = (state) => state.categories.categories
 //     .reduce((acc,category) => {
--- src/store/store.ts
+++ src/store/store.ts
@@ -1,5 +1,5 @@
-import { compose, createStore, applyMiddleware} from 'redux';
-import { persistStore, persistReducer} from 'redux-persist'
+import { compose, createStore, applyMiddleware, Middleware} from 'redux';
+import { persistStore, persistReducer, PersistConfig} from 'redux-persist'
 import logger from 'redux-logger';
 import storage from 'redux-persist/lib/storage'
 // import thunk from 'redux-thunk'
@@ -8,16 +8,29 @@ import createSagaMiddleware from 'redux-saga'
 import { rootReducer } from './root-reducer';
 import { rootSaga } from './rootSaga';
 
+declare global{
+    interface Window {
+        __REDUX_DEVTOOLS_EXTENSION_COMPOSE__ : typeof compose
+    }
+}
+
+export type ExtendedPersistConfig = PersistConfig<RootState> & {
+    whitelist : (keyof RootState)[]
+}
+
+export type RootState = ReturnType<typeof rootReducer>
+
 const sagaMiddleware = createSagaMiddleware()
 
-const middleWares = [process.env.NODE_ENV === 'development' && logger, sagaMiddleware].filter(Boolean)
+const middleWares = [process.env.NODE_ENV === 'development' && logger, sagaMiddleware]
+.filter((middleware) : middleware is Middleware  => Boolean(middleware))
 
 const composedEnhancer = (process.env.NODE_ENV === 'development' && window && window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__) || compose
 
 const composedEnhancers = composedEnhancer(applyMiddleware(...middleWares))
 
-const persistConfig = {
-    key: rootReducer,
+const persistConfig : ExtendedPersistConfig = {
+    key: 'root',
     storage,
     whitelist: ['cart']
 }
