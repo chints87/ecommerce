526a442b79fa98910da7718a2e8771bdb12bd120
Chintan <cgandhi4@gmail.com>
2022-09-16 18:02:17 +0530

    PWA config

--- src/index.js
+++ src/index.js
@@ -11,6 +11,7 @@ import { UserProvider } from './context/user';
 import { CategoryProvider } from './context/category';
 import { CartIconProvider } from './context/cartIcon';
 import { Elements } from '@stripe/react-stripe-js'
+import * as serviceWorker from './service-worker'
 
 
 import { store, persistor } from './store/store'
@@ -38,6 +39,7 @@ root.render(
   </React.StrictMode>
 );
 
+serviceWorker.register()
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
