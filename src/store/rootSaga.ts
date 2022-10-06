f6549fefbe875c33f3d7d731c05235406bc5cb03
Chintan <cgandhi4@gmail.com>
2022-10-06 16:52:48 +0530

    Add comments

--- src/store/rootSaga.ts
+++ src/store/rootSaga.ts
@@ -3,7 +3,7 @@ import { all, call } from 'typed-redux-saga/macro'
 import { categoriesSaga } from './categories/categorySaga'
 import { userSaga } from './user/userSaga'
 
-
+// Encapsulate all sagas
 export function* rootSaga(){
     yield* all([call(categoriesSaga), call(userSaga)])
 }
